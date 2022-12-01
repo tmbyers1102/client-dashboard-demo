3be144353e8ba14399c7aef4de32f4fb1ef46af7
Tom Byers <tmbyers1102@gmail.com>
2022-11-30 21:50:23 -0800

    added questions content

--- src/Components/Tasks/TaskTable.jsx
+++ src/Components/Tasks/TaskTable.jsx
@@ -59,7 +59,6 @@ const TaskTable = () => {
                             task={task}
                         />
                     ))}
-                    
                 </TableBody>
                 </Table>
             </Card>
