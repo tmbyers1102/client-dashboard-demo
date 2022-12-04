c4196118b13c6f2660aeb605219b8626f5a13d6d
Tom Byers <tmbyers1102@gmail.com>
2022-12-04 10:57:41 -0800

    updated model test

--- src/Components/Promotions/Promotions.jsx
+++ src/Components/Promotions/Promotions.jsx
@@ -104,12 +104,11 @@ const Promotions = () => {
                                 </div>
                                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                 <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                                    Deactivate account
+                                    Test Popup
                                 </Dialog.Title>
                                 <div className="mt-2">
                                     <p className="text-sm text-gray-500">
-                                    Are you sure you want to deactivate your account? All of your data will be permanently
-                                    removed. This action cannot be undone.
+                                    testing testing testing
                                     </p>
                                 </div>
                                 </div>
@@ -121,7 +120,7 @@ const Promotions = () => {
                                 className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                                 onClick={() => setOpen(false)}
                             >
-                                Deactivate
+                                some other option
                             </button>
                             <button
                                 type="button"
