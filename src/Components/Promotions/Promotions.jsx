b2308beeb1fad2e11fa25b411c1c29f47ef43cf8
Tom Byers <tmbyers1102@gmail.com>
2023-01-03 13:39:18 -0800

    removed popup button on promotions

--- src/Components/Promotions/Promotions.jsx
+++ src/Components/Promotions/Promotions.jsx
@@ -70,14 +70,14 @@ const Promotions = () => {
                         ))}
                     </TableBody>
                 </Table>
-                <button
+                {/* <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={() => setOpen(true)}
                     ref={cancelButtonRef}
                 >
                     Cancel
-                </button>
+                </button> */}
             </Card>
             <Transition.Root show={open} as={Fragment}>
                 <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
